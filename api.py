7ef1cdc0cd0f59ded798717006eac815f443393f
SietseHuisman <sietsehuisman@hotmail.com>
2015-05-25 14:26:11 +0200

    added /api/identity
    returns direct json

--- api.py
+++ api.py
@@ -30,15 +30,11 @@ def first_page():
     app.logger.debug('You arrived at ' + url_for('first_page'))
     return render_template('LOD_Identity_Home.html')
 
-@app.route('/get_similar')
-def get_similar():
-    
+
+def get_similar(uri):
     app.logger.debug('You arrived at /get_similar')
-    app.logger.debug('I received the following uri: ' + str(request.args.get("uri",None) ))
+    app.logger.debug('I received the following uri: ' +  uri)
     
-    uri = request.args.get('uri', None)
-    
-    return_format = request.args.get('format', None)
     query = "select ?p where {" + uri + " <http://www.w3.org/2002/07/owl#sameAs> ?p}"
     endpoint = REPOSITORY
     sparql = SPARQLWrapper(endpoint)
@@ -51,15 +47,15 @@ def get_similar():
     app.logger.debug('Query:\n{}'.format(query))        
 
     try :
-            response = sparql.query().convert()
-            app.logger.debug('Results were returned')
-            app.logger.debug(response)
-          
-            for item in response['results']['bindings']:
-                if item['p']['value'] in to_return:
-                    to_return[item['p']['value']] +=1
-                else:
-                    to_return[item['p']['value']] = 1
+        response = sparql.query().convert()
+        app.logger.debug('Results were returned')
+        app.logger.debug(response)
+      
+        for item in response['results']['bindings']:
+            if item['p']['value'] in to_return:
+                to_return[item['p']['value']] +=1
+            else:
+                to_return[item['p']['value']] = 1
             
             
     except Exception as e:
@@ -71,12 +67,18 @@ def get_similar():
     else :
         return jsonify(to_return)
     
+@app.route('/api/identity', methods=['GET'])
+def return_similar_api():
+    return get_similar(str(request.args.get("uri",None)))
     
+
+'''    
 @app.route('/api/<path:uri>', methods=['GET'])
 def get_task(uri):
     if len(uri) == 0:
         abort(404)
     return jsonify({'connections': connections})
+'''
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/functions.js
+++ static/js/functions.js
@@ -1,12 +1,12 @@
 function get_similar(){
             var uri = document.getElementById("url").value
             var format = "RDF"
-            $.get('/get_similar',data={'uri': uri, 'format': format}, function(data){
+            $.get('/api/identity',data={'uri': uri, 'format': format}, function(data){
                 var responseString = "";
                 for (URI in data) {
                     responseString += (URI + ": " + data[URI] + "<br>");
                 }
-                console.log(responseString);
+                //console.log(responseString);
                 document.getElementById('response').innerHTML = responseString;
 	    });
 	

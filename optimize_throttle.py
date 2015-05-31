7c02e71e9c7ba869e4c4d5e748f97db552d8e766
tomrijntjes <tomrijntjes@gmail.com>
2015-05-31 21:13:44 +0200

    final changes

--- optimize_throttle.py
+++ optimize_throttle.py
@@ -1,11 +1,26 @@
 from retriever import Retriever
 import time
+import urllib.request
 
-#urls = ["http://index.lodlaundromat.org/r2d/http%3A%2F%2Fwww.w3.org%2F2002%2F07%2Fowl%23sameAs?page="+str(_) for _ in range(20)]
-urls = ["https://www.google.nl/?gfe_rd=cr&ei=4GRnVcXfIsb--Qby54GIBA#q=1&safe=off&start="+str(_) for _ in range (0,500,10)]
+from operator import itemgetter
 
 
-for i in range(1,50):
+
+urls = ["http://index.lodlaundromat.org/r2d/http%3A%2F%2Fwww.w3.org%2F2002%2F07%2Fowl%23sameAs?page="+str(_) for _ in range(10)]
+#urls = ["https://www.google.nl/?gfe_rd=cr&ei=4GRnVcXfIsb--Qby54GIBA#q=1&safe=off&start="+str(_) for _ in range (0,500,10)]
+
+
+results = list()
+
+
+for i in range(1,20):
   t=time.clock()
   Retriever(urls,'n',i)
-  print("Throttle {0} fetches 20 urls in {1}".format(i,time.clock()-t))
+  result = time.clock()-t
+  results.append(result)
+  print("Throttle {0} fetches {1} urls with {2} per url".format(i,len(urls),(result)/len(urls)))
+
+print(min(results))
+best = min(enumerate(results), key=itemgetter(1))[0]+1
+print(best)
+print("Throttle {0} achieves the best result: {1} as fast".format(best,1/((results[0]-min(results))/results[0])))
